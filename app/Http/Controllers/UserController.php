b8dc14228bb67cab6f3698e661ac9f314d7cb97f
arianne05 <quimpoarianne5@gmail.com>
2023-11-26 11:15:42 +0800

    Created new controller and sample.blade.php and connect them

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,37 @@ class UserController extends Controller
     }
 
     public function passParameter($id){
-        return 'Argument Passed: '.$id;
+
+        // WAYS TO CONNECT CONTROLLER TO VIEWS FOLDER
+        
+        /* Option 1 
+        $data =  array( //sample database in an array
+            "id" => $id,
+            "name" => "Arianne",
+            "age" => 22,
+            "email" => "arianne@email.com"
+        );
+        return view('sample', $data); //pass data into sample.blade.php file in views folder
+        */
+
+        /* Option 2 
+        return view('sample')
+            ->with('name', 'Arianne')
+            ->with('age', 22)
+            ->with('id', $id)
+            ->with('email', 'arianne@email.com');
+        */
+
+        /* Option 3 */
+        //you can include a set of different database and join with another single data
+        $data = ["data" => "data from database"];
+
+        return view('sample')
+            ->with('data', $data)
+            ->with('name', 'Arianne')
+            ->with('age', 22)
+            ->with('id', $id)
+            ->with('email', 'arianne@email.com');
+
     }
 }
