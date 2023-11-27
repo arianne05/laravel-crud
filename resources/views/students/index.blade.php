fe4f4e7d4a376d695f9706e9f87a007559cffb67
arianne05 <quimpoarianne5@gmail.com>
2023-11-27 11:15:51 +0800

    Install tailwind

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Student List</title>
 </head>
 <body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    
     <h1>List of Student</h1>
     @foreach ($counts as $count)
         <p><b>{{$count->gender}}</b> {{$count->gender_count}}</p>
