4447e6da120d8058a352d439f28068b85b82bc82
arianne05 <quimpoarianne5@gmail.com>
2023-11-28 12:21:50 +0800

    Added alpine.js for resposive toggle of menu burger

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+
+    <script src="//unpkg.com/alpinejs" defer></script>
     <title>Login</title>
     {{-- <title>{{$title}}</title><!--Access the passed argument from index if exister if not error will occur--> --}}
 </head>
