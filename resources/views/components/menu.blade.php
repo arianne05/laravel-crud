04c92b3fd9c74bf0a65fd53da1d1c870810bcd61
arianne05 <quimpoarianne5@gmail.com>
2023-11-30 17:08:51 +0800

    Added auth condition to display logout if logged in and hide the login and register btn

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,8 +1,12 @@
 <ul class="flex flex-col md:flex-row px-4">
-    <li><a href="login" class="block py-2 pr-4 pl-3">Login</a></li>
-    <li><a href="register" class="block py-2 pr-4 pl-3">Register</a></li>
-    <form action="/logout" method="POST">
-        @csrf
-        <li><button class="block py-2 pr-4 pl-3">Logout</button></li>
-    </form>
+    @auth {{--added auth condition| if authenticated or logged in display logout only--}}
+        <li><a href="/newstudent" class="block py-2 pr-4 pl-3">Add User</a></li>
+        <form action="/logout" method="POST">
+            @csrf
+            <li><button class="block py-2 pr-4 pl-3">Logout</button></li>
+        </form>
+    @else
+        <li><a href="login" class="block py-2 pr-4 pl-3">Login</a></li>
+        <li><a href="register" class="block py-2 pr-4 pl-3">Register</a></li>
+    @endauth
 </ul>
