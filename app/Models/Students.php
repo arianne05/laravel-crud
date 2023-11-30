f1a22a41b91f3674addbaa886355fd44d255eaed
arianne05 <quimpoarianne5@gmail.com>
2023-11-30 17:41:13 +0800

    Added protected $guarded in the Student models to execute crud in the student db

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Students extends Model
-{
+{   
+    protected $guarded = []; // $guarded allow to manipulate all data in the students database/table
+
+    /* 
+    protected $fillable = [ // $fillable allow to manipulate a certain column in the students table in the db
+        'first_name', 
+        'last_name', 
+        'email', 
+        'age', 
+        'gender'
+    ];
+    */
+    
     use HasFactory;
 }
