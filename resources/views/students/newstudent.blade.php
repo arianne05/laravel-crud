9ce7785949cccfd9900575ddb631249dcbda321e
arianne05 <quimpoarianne5@gmail.com>
2023-11-30 17:40:29 +0800

    Added error handling and old function in all input

--- resources/views/students/newstudent.blade.php
+++ resources/views/students/newstudent.blade.php
@@ -10,40 +10,60 @@
         
         <section class="mt-10">
 
-            <form action="/process_signin" method="POST" class="flex flex-col">
+            <form action="/newstudent" method="POST" class="flex flex-col"> <!--process within the same oage-->
                 @csrf
                 <div class="mb-6 pt-3 rounded bg-gray-200">
                     <label for="first_name" class="block text-gray-700 text-sm font-bold mb-2 ml-3">Firstname</label>
-                    <input type="text" name="first_name" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
+                    <input type="text" name="first_name" value="{{old('first_name')}}" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
+                    @error('first_name')
+                        <p class="text-red-500 text-xs p-2">
+                            {{$message}}
+                        </p>
+                    @enderror   
                 </div>
 
                 <div class="mb-6 pt-3 rounded bg-gray-200">
                     <label for="last_name" class="block text-gray-700 text-sm font-bold mb-2 ml-3">Lastname</label>
-                    <input type="text" name="last_name" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
+                    <input type="text" name="last_name" value="{{old('last_name')}}" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
+                    @error('last_name')
+                        <p class="text-red-500 text-xs p-2">
+                            {{$message}}
+                        </p>
+                    @enderror   
                 </div>
 
                 <div class="mb-6 pt-3 rounded bg-gray-200">
                     <label for="gender" class="block text-gray-700 text-sm font-bold mb-2 ml-3">Gender</label>
-                    <select type="text" name="gender" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
-                        <option value=""></option>
-                        <option value="Male">Male</option>
-                        <option value="Female">Female</option>
+                    <select type="text" name="gender" value="{{old('gender')}}" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
+                        <option value="" {{old('gender')=="" ? 'selected':''}}></option>
+                        <option value="Male" {{old('gender')=="Male" ? 'selected':''}}>Male</option>
+                        <option value="Female" {{old('gender')=="Female" ? 'selected':''}}>Female</option>
                     </select>
+                    @error('gender')
+                        <p class="text-red-500 text-xs p-2">
+                            {{$message}}
+                        </p>
+                    @enderror   
                 </div>
 
                 <div class="mb-6 pt-3 rounded bg-gray-200">
                     <label for="age" class="block text-gray-700 text-sm font-bold mb-2 ml-3">Age</label>
-                    <input type="number" name="age" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
+                    <input type="number" name="age" value="{{old('age')}}" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
+                    @error('age')
+                        <p class="text-red-500 text-xs p-2">
+                            {{$message}}
+                        </p>
+                    @enderror   
                 </div>
 
                 <div class="mb-6 pt-3 rounded bg-gray-200">
                     <label for="email" class="block text-gray-700 text-sm font-bold mb-2 ml-3">Email</label>
                     <input type="email" name="email" value="{{old('email')}}" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
-                </div>
-
-                <div class="mb-6 pt-3 rounded bg-gray-200">
-                    <label for="password" class="block text-gray-700 text-sm font-bold mb-2 ml-3">Password</label>
-                    <input type="password" name="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-400 px-3">
+                    @error('email')
+                        <p class="text-red-500 text-xs p-2">
+                            {{$message}}
+                        </p>
+                    @enderror   
                 </div>
                 
                 <button type="submit" class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200">
